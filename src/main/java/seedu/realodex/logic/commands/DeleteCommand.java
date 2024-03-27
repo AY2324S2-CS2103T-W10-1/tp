a9df76d1dbdb9ca881811a881258e363488da6f0
jyztintan <justintanwk2001@gmail.com>
2024-03-27 11:38:20 +0800

    Standardise list view behavior post-delete command
    Calling the delete command on a filtered list currently retains the filtered view, diverging from the expected behavior observed with other commands.
    
    This inconsistency in command behavior can lead to confusion and a disjointed user experience, as users expect uniform behavior across similar functionalities.
    
    Modify the delete command logic to revert the view back to the original, unfiltered list post-deletion. Ensure this change aligns with the standard behavior observed in other commands for consistency.
    
    Standardizing the behavior post-command execution enhances user predictability and alignment with established interface patterns, improving overall usability.

--- src/main/java/seedu/realodex/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/realodex/logic/commands/DeleteCommand.java
@@ -66,6 +66,7 @@ public class DeleteCommand extends Command {
         }
 
         model.deletePerson(personToDelete);
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
 
